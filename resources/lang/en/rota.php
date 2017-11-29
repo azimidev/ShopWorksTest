1ef9ccbcf1b1a746be6b902f5398e3ff011d9ba2
Hassan Azimi <hazz.azimi@gmail.com>
2017-11-29 22:10:45 +0000

    Add rota.php

---
+++ resources/lang/en/rota.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+	'rota_slot_staff'      => 'Rota slot staff',
+	'day'                  => 'Day',
+	'staff_id'             => 'Staff id',
+	'staff_and_times'      => 'Staff id (shift times)',
+	'total_hours_worked'   => 'Total hours worked',
+	'total_minutes_worked' => 'Total minutes worked',
+
+	'day_of_week' => [
+		'monday'    => 'Monday',
+		'tuesday'   => 'Tuesday',
+		'wednesday' => 'Wednesday',
+		'thursday'  => 'Thursday',
+		'friday'    => 'Friday',
+		'saturday'  => 'Saturday',
+		'sunday'    => 'Sunday',
+	],
+];
