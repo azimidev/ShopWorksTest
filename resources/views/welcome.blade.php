4ff44aa14a59a5c08a8172e22e1152c283a92ed0
Hassan Azimi <hazz.azimi@gmail.com>
2017-11-30 00:47:52 +0000

    Display total hours

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,27 @@
 @extends('layouts.app')
 @section('content')
 	<div class="container">
+		<h1 class="page-header">Staff Slot Rota</h1>
 		<table class="table table-bordered table-responsive">
-			<thead>
+			@foreach ($days as $key => $value)
 				<tr>
-					<th>Days</th>
+					<th>{{ $key }}</th>
+					@foreach ($value as $day)
+						<td>
+							<span class="badge">STAFF {{ $day->staffid }}</span> <br>
+							<span class="label label-success">{{ $day->starttime }}</span>
+							<span class="label label-danger">{{ $day->endtime }}</span>
+						</td>
+					@endforeach
 				</tr>
-			</thead>
-			<tbody>
-				@foreach ($days as $key => $value)
-					<tr>
-						<th>{{ $key }}</th>
-						@foreach ($value as $day)
-							<td>
-								<span class="badge">STAFF {{ $day->staffid }}</span> <br>
-								<span class="label label-success">{{ $day->starttime }}</span>
-								<span class="label label-danger">{{ $day->endtime }}</span>
-							</td>
-						@endforeach
-					</tr>
-				@endforeach
-			</tbody>
+			@endforeach
 		</table>
+		<h1 class="page-header">Total Hours Worked This week</h1>
 		<table class="table table-bordered table-responsive">
 			<tr>
 				@foreach ($total as $key => $value)
 					<th>
-						{{ $key }} Total Hours: {{ $value['hours'] }}
+						{{ $key }} Total Hours: <span class="badge">{{ $value['hours'] }}</span>
 					</th>
 				@endforeach
 			</tr>
