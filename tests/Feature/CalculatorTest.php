7f13396df39f312a16a63d68c9f703162541f0c3
Hassan Azimi <hazz.azimi@gmail.com>
2017-11-30 18:36:18 +0000

    Add test

--- tests/Feature/CalculatorTest.php
+++ tests/Feature/CalculatorTest.php
@@ -2,17 +2,49 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\RotaSlotStaff;
 use Tests\TestCase;
 
 class CalculatorTest extends TestCase
 {
-	use RefreshDatabase;
-
 	/** @test */
 	public function application_works()
 	{
-		$this->get('/')
-		     ->assertSuccessful();
+		$this->get('/')->assertSuccessful();
+	}
+
+	/** @test */
+	public function a_week_has_seven_days()
+	{
+		$days = RotaSlotStaff::valid()->get()->groupBy(function ($column) {
+			return intToDayOfWeek($column->daynumber);
+		});
+
+		$this->assertCount(7, $days->toArray());
+	}
+
+	/** @test */
+	public function int_to_day_of_the_weeks()
+	{
+		$monday = intToDayOfWeek(0);
+		$this->assertEquals('Monday', $monday);
+
+		$tuesday = intToDayOfWeek(1);
+		$this->assertEquals('Tuesday', $tuesday);
+
+		$wednesday = intToDayOfWeek(2);
+		$this->assertEquals('Wednesday', $wednesday);
+
+		$thursday = intToDayOfWeek(3);
+		$this->assertEquals('Thursday', $thursday);
+
+		$friday = intToDayOfWeek(4);
+		$this->assertEquals('Friday', $friday);
+
+		$saturday = intToDayOfWeek(5);
+		$this->assertEquals('Saturday', $saturday);
+
+		$sunday = intToDayOfWeek(6);
+		$this->assertEquals('Sunday', $sunday);
 	}
 }
